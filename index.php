9d0b9a3dbf090a5accc649f34f29ac12e33e8e50
Peter Sharp <peter@petersharp.co.nz>
2015-07-10 11:51:35 +1200

    decided not to write some crazy template co-ordinator but instead just import header and footer on each page. home.php is no longer needed.

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?
 include('./templates/header.php');
-include('home.php');
+?>
+<main>
+  <div class="featured_event"><img src="" alt="" /></div>
+  <h1> <strong>Next time:</strong> A changed life </h1>
+  <article class="CTA_area">
+  </header>
+    <p>Whether you are new to Christchurch New Zealand or have been for a long time, Rodem House
+      is a place where you can:
+    </p>
+  </header>
+  <p>
+    <ul>
+      <li>-find</li>
+      <li></li>
+    </ul>
+  </p>
+  </article>
+</main>
+<?
 include('./templates/footer.php');
 ?>
