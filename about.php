7f3d97b7e8410d9137a7d75fa1aee9cd1f4d8039
Peter Sharp <peter@petersharp.co.nz>
2015-07-11 18:02:58 +1200

    finished all html

---
+++ about.php
@@ -0,0 +1,15 @@
+<?
+include('./templates/header.php');
+?>
+<main>
+  <h1>about us</h1>
+  <article class="about">
+    <h2>who we are</h2>
+    <p>Rodem House is a charitable trust organization <span class="support">(Registration No: CC10913)</span> to support missionaries
+      and pastors who need rest and refreshment. Also we support international people who come to
+      Christchurch. Rodem house is a faith mission operated by prayer and the support of Christians.</p>
+  </article>
+</main>
+<?
+include('./templates/footer.php');
+?>
