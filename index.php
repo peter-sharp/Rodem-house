7f1eabae2efdb4deee90ca87c83521a20202177f
Peter Sharp <peter@petersharp.co.nz>
2015-07-11 17:06:40 +1200

    removed next event from home page, removed english lessons CTA button, changed CTA text

--- index.php
+++ index.php
@@ -2,20 +2,19 @@
 include('./templates/header.php');
 ?>
 <main>
-  <div class="featured_event"><img src="" alt="" /></div>
-  <h1> <strong>Next time:</strong> A changed life </h1>
+
+  <h1>international<br>fellowship</h1>
   <article class="CTA_area">
-  </header>
-    <p>Whether you are new to Christchurch New Zealand or have been for a long time, Rodem House
-      is a place where you can:
-    </p>
-  </header>
-  <p>
-    <ul>
-      <li>-find</li>
-      <li></li>
-    </ul>
-  </p>
+    <div class="regular wrapper">
+
+      <p>
+          Come to Rodem fellowship to:<br>
+          practice your <strong>English,</strong>
+          make international friends,<br>
+          and find out about <strong>Christianity</strong>
+      </p>
+      <button type="button" class="meeting">next meeting</button>
+    </div>
   </article>
 </main>
 <?
