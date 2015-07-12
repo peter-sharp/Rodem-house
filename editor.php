9388f531b32028e0bba0904471ea1fda414e37d0
Peter Sharp <peter@petersharp.co.nz>
2015-07-13 09:56:18 +1200

    initally added button element for add event button but changed to anchor due to it being better suited to the task

--- editor.php
+++ editor.php
@@ -7,7 +7,8 @@ include('./templates/header.php');
     <p class="admin">This is where you can make changes to the website. you can make changes to the text
       on the pages listed above. </p>
       <p>To add a new fellowship or social event<br>Click this button</p>
-    <p> <small>You can also add, update or delete felowship or social events. </small></p>
+      <a class="btn btn_CTA" href="edit_events.php">add event</a>
+    <p> <small>You can also add, update or delete felowship or social events by clicking on the 'events' link above. </small></p>
   </article>
 </main>
 <?php
