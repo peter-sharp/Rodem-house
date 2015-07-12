c54b835210215845d6b17033367e1f7d4a623a32
Peter Sharp <peter@petersharp.co.nz>
2015-07-12 20:31:13 +1200

    added html body

---
+++ contact.php
@@ -0,0 +1,28 @@
+<?php
+include('./templates/header.php');
+?>
+<main>
+
+  <h1>contact</h1>
+  <article class="contact" itemscope itemtype="http://schema.org/Person">
+    <div class="site wrapper">
+      <p>
+        <img src="" alt="" /> 022 333 4444
+      </p>
+      <p>
+        <img src="" alt="" /> 03 444 5556
+      </p>
+      <p>
+        <img src="" alt="" /> person@rodemhouse.org
+      </p>
+      <p>
+        <img src="" alt="" /> Some St, Suburb, CHristchurch
+      </p>
+      <div class="map"></div>
+    </div>
+  </article>
+
+</main>
+<?php
+include('./templates/footer.php');
+?>
