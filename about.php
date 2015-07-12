e81ae34a377a30c6759fea993e78fb8306ed9f1b
Peter Sharp <peter@petersharp.co.nz>
2015-07-12 20:30:40 +1200

    changed span tag to small tag

--- about.php
+++ about.php
@@ -1,15 +1,15 @@
-<?
+<?php
 include('./templates/header.php');
 ?>
 <main>
   <h1>about us</h1>
   <article class="about">
     <h2>who we are</h2>
-    <p>Rodem House is a charitable trust organization <span class="support">(Registration No: CC10913)</span> to support missionaries
+    <p>Rodem House is a charitable trust organization <small>(Registration No: CC10913)</small> to support missionaries
       and pastors who need rest and refreshment. Also we support international people who come to
       Christchurch. Rodem house is a faith mission operated by prayer and the support of Christians.</p>
   </article>
 </main>
-<?
+<?php
 include('./templates/footer.php');
 ?>
