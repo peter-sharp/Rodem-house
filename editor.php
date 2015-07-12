2c6353733fb03f34468716bbd59bf0bef443a15c
Peter Sharp <peter@petersharp.co.nz>
2015-07-12 21:59:20 +1200

    almost finished markup

---
+++ editor.php
@@ -0,0 +1,15 @@
+<?php
+include('./templates/header.php');
+?>
+<main>
+  h1{website editor}
+  <article class="editor">
+    <p class="admin">This is where you can make changes to the website. you can make changes to the text
+      on the pages listed above. </p>
+      <p>To add a new fellowship or social event<br>Click this button</p>
+    <p> <small>You can also add, update or delete felowship or social events. </small></p>
+  </article>
+</main>
+<?php
+include('./templates/footer.php');
+?>
