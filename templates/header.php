735a3cc3f02a96bcef1f46dbc8b8cae60d482157
Peter Sharp <peter@petersharp.co.nz>
2015-07-12 20:35:00 +1200

    added php after the first opening <? php tag, wouldn't run before that

--- templates/header.php
+++ templates/header.php
@@ -1,3 +1,12 @@
+<?php
+    $pages = array(
+      'index' => 'home',
+      'about' => 'about us',
+      'meetings' => 'meetings',
+      'contact' => 'contact',
+      'english' => '<small>free</small></br>English lessons'
+    );
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,3 +14,13 @@
   <title>Rodem House</title>
 </head>
 <body>
+  <header>
+    <h1 class="logo">Rodem House</h1>
+    <nav>
+      <ul>
+        <?php foreach ($pages as $pageId => $pageTitle): ?>
+        <li <?=(($_GET['pg'] == $pageId) ? 'class="active"' : '')?>><a href="./<?=$pageId?>.php" ><?=$pageTitle?></a></li>
+        <?php endforeach; ?>
+      </ul>
+    </nav>
+  </header>
