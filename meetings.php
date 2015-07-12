99e72dee2e0552f86e75b962c74a6c4de21e8729
Peter Sharp <peter@petersharp.co.nz>
2015-07-12 21:36:58 +1200

    finished initial markup

---
+++ meetings.php
@@ -0,0 +1,38 @@
+<?php
+include('./templates/header.php');
+?>
+<main>
+  <h1>meetings</h1>
+  <article class="meetings">
+    <div class="site wrapper">
+      <h2>international fellowship</h2>
+      <section class="fellowship accordion">
+        <p>Enjoy a meal, friendship, and learn more about
+          Christianity every Tuesday at 6pm.</p>
+        <div class="event">
+          <h3>This week's event</h3>
+          <P>This is the description for what will happen next Rodem fellowship.
+          This could be a game night, guest speaker or any other type of event. It should
+          be a reasonable description but not too long or else people might loose interest.</P>
+          <p class="map_caption">lessons are at 12 Some St</p>
+          <div class="map"></div>
+        </div>
+      </section>
+      <h2>social event</h2>
+      <section class="social accordion">
+        <div class="event">
+          <h3>Come ice-skating with us</h3>
+          <P>This is the description for the up-comming social event.
+          Here you can put some text to interest people in coming an other details
+          like what they need to bring, what will happen if itrains etc.</P>
+          <p class="map_caption">Be there by 5:45pm at Some other St</p>
+          <div class="map"></div>
+        </div>
+
+      </section>
+    </div>
+  </article>
+</main>
+<?php
+include('./templates/footer.php');
+?>
