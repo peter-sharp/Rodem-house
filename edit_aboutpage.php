1f36a4ffbb2efe1f213de8059dfbaba83adc66ed
Peter Sharp <peter@petersharp.co.nz>
2015-07-13 20:45:30 +1200

    finished initial markup

--- edit_aboutpage.php
+++ edit_aboutpage.php
@@ -2,6 +2,19 @@
 include('./templates/header.php');
 ?>
 <main>
+  <form class="pages" action="index.html" method="post">
+
+  <main>
+    <h1>edit<br><small>about us page</small></h1>
+    <p>website editor > about page</p>
+    <section class="editor">
+      <p>loged in as ADMIN</p>
+      <label for="about">about us text</label>
+      <textarea  name="about" ></textarea>
+
+      <a class="btn btn-back" name="change">back</button>
+      <button class="btn btn-CTA" type="submit" name="change">change</button>
+    </section>
 </main>
 <?php
 include('./templates/footer.php');
