636d0b81d60873647f6f4193d97639a97dce9934
Peter Victor Sharp <petersharp@Peters-Mac-mini.local>
2015-07-13 13:48:23 +1200

    added editor footer template

--- templates/footer.php
+++ templates/footer.php
@@ -1,3 +1,15 @@
+<?php
+$editorPage = false;
+ if ($editorPage === true):?>
+  <footer>
+    <p>What would you like to do with the selected activities?</p>
+    <button type="button">delete</button>
+    <button type="button">view</button>
+    <button type="button">edit</button>
+  </footer>
+</form>
+<?php else:?>
 <footer> <span>&copy; Rodem House</span> <a href="editor.php">login</a> </footer>
+<?php endif?>
 </body>
 </html>
