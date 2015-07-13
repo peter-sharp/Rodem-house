5bc04229e294f938f2c27ba085bc2b0277a8866e
Peter Sharp <peter@petersharp.co.nz>
2015-07-13 20:42:12 +1200

    finished initial markup

--- edit_homepage.php
+++ edit_homepage.php
@@ -1,7 +1,19 @@
 <?php
 include('./templates/header.php');
 ?>
+<form class="pages" action="index.html" method="post">
+
 <main>
+  <h1>edit<br><small>home page</small></h1>
+  <p>website editor > home page</p>
+  <section class="editor">
+    <p>loged in as ADMIN</p>
+    <label for="intro">introduction text</label>
+    <textarea  name="intro" ></textarea>
+
+    <a class="btn btn-back" name="change">back</button>
+    <button type="submit" name="change">change</button>
+  </section>
 </main>
 <?php
 include('./templates/footer.php');
