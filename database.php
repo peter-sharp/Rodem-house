8e37106d0d64c89ee889029aae6cb618f4250689
Peter Sharp <Peter@petersharp.co.nz>
2015-07-16 11:15:00 +1200

    added ability to connect and insert data

--- database.php
+++ database.php
@@ -2,7 +2,7 @@
 #TODO require_once('authenticator.php');
 #TODO require_once('logger.php');
 
-class databaseException extends Exception { };
+class DatabaseException extends Exception { };
 /**
   * helper class containing various database methods
   */
@@ -12,6 +12,10 @@ class DatabaseHelper {
 
   /**
     * constructor method that initialises the database connection
+    * @param string $server the server the database belongs to
+    * @param string $username the username for the database
+    * @param string $password the password for the database
+    * @param string $database the database to connect to
     */
   function DatabaseHelper($server = null, $username = null, $password = null, $database = null){
     $server   = ();
@@ -44,5 +48,46 @@ class DatabaseHelper {
 
     return $mysqli;
   }
+
+  /**
+    * inserts a single row of data into a database based on params given and redirect
+    * user on success/failure with message/error in URL parameter. Note: only accepts strings.
+    *
+    * @param string $table Name of table to insert new row into
+    * @param array $formData Array of data to insert into new row of table
+    *
+    *
+    */
+  private function insert($table, $formData){
+    $mysqli = $this->mysqli;
+
+    foreach( $formData as $key => $value){
+      $values[] = $value;
+      $slots[] = "?";
+      $keys[] = $key;
+    }
+
+
+    $sql = "INSERT INTO `$table`(". implode(',', $keys) .") VALUES (". implode(',', $slots) .")";
+    $statement = $mysqli->prepare($sql);
+
+    // since all inserted values are going to be strings @TODO in future should make this take more data types
+    $types = str_repeat("s",count($values));
+
+    // function to insert the contents of an array as arguments to the bind_param function
+    $query = call_user_func_array(array($statement, "bind_param"), array_merge(array($types), $values) );
+
+    // generate header message
+    if(!$query){ //throw a mysql error
+      throw new DatabaseException( "Error in mysql exception: ".$mysqli->error($query));
+      $message = "Error=Failed to add (".implode(',',$values).")";
+    }
+    else {
+      $message = "message=Successfully added (".implode(',',$values).")";
+    }
+
+    //
+  }
+
 }
 ?>
