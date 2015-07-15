846c28cea5ece3b36409cf0874da91b6a2e121f7
Peter Sharp <Peter@petersharp.co.nz>
2015-07-15 22:06:27 +1200

    inital commit: began properties

---
+++ authenticator.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+#TODO require_once('database.php');
+#TODO require_once('logger.php');
+
+class AuthenticatorException extends Exception { };
+/**
+  * Authenticator helper class
+  **/
+
+class AuthenticatorHelper {
+  private $db;
+  #TODO private $logger;
+
+}
+?>
