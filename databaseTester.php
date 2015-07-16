07d32940f124d4154f52616b78f10c75c7b6eef6
Peter Sharp <Peter@petersharp.co.nz>
2015-07-17 11:07:22 +1200

    removed stray colon

--- databaseTester.php
+++ databaseTester.php
@@ -12,7 +12,7 @@ class DatabaseTest extends PHPUnit_Framework_Testcase {
   public function testCanInsertIntoDatabase(){
     $databaseHelper = new DatabaseHelper();
 
-    $databaseHelper->insert("pages", array("contact", "parking can be found around the back.", 3, 1));:
+    $databaseHelper->insert("pages", array("contact", "parking can be found around the back.", 3, 1));
 
     $result = $mysqli->query("SELECT title, body, contacts_id, edited_by FROM `pages` WHERE `title` = 'contact'");
     $contact_page = $result->fetch_assoc();
