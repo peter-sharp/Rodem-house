57da22802521443b9a810f84dc078cc8decf7be6
Peter Sharp <Peter@petersharp.co.nz>
2015-07-16 22:12:22 +1200

    initial commit: tests connection to database and insertion of data into table

---
+++ databaseTester.php
@@ -0,0 +1,22 @@
+<?php
+require("database.php");
+
+class DatabaseTest extends PHPUnit_Framework_Testcase {
+
+  public function testCanConnectTODatabase(){
+    $databaseHelper = new DatabaseHelper();
+
+    $this->assertInstanceOf('DatabaseHelper' ,$databaseHelper);
+  }
+
+  public function testCanInsertIntoDatabase(){
+    $databaseHelper = new DatabaseHelper();
+
+    $databaseHelper->insert("pages", array("contact", "parking can be found around the back.", 3, 1));:
+
+    $result = $mysqli->query("SELECT title, body, contacts_id, edited_by FROM `pages` WHERE `title` = 'contact'");
+    $title = $result->fetch_assoc();
+    $this->assertEquals($title , "contact");
+  }
+}
+?>
