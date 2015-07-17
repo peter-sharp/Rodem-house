5281cee1fa1962d5b406e2dec83da1b4eedb9426
Peter Sharp <peter@petersharp.co.nz>
2015-07-17 15:33:42 +1200

    tests with associative array now

--- databaseTester.php
+++ databaseTester.php
@@ -2,7 +2,10 @@
 require("database.php");
 
 class DatabaseTest extends PHPUnit_Framework_Testcase {
-
+  public $server = "localhost";
+  public $username = "root";
+  public $password = "";
+  public $database = "website";
   public function testCanConnectTODatabase(){
     $databaseHelper = new DatabaseHelper();
 
@@ -11,8 +14,8 @@ class DatabaseTest extends PHPUnit_Framework_Testcase {
 
   public function testCanInsertIntoDatabase(){
     $databaseHelper = new DatabaseHelper();
-
-    $databaseHelper->insert("pages", array("contact", "parking can be found around the back.", 3, 1));
+    $mysqli = new mysqli($this->server, $this->username, $this->password, $this->database);
+    $databaseHelper->insert("pages", array("title" => "contact", "body" => "parking can be found around the back.", "contacts_id" => 3, "edited_by" => 1));
 
     $result = $mysqli->query("SELECT title, body, contacts_id, edited_by FROM `pages` WHERE `title` = 'contact'");
     $contact_page = $result->fetch_assoc();
