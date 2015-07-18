a08307ef68f40bb57682af2c675070860b92132c
Peter Sharp <Peter@petersharp.co.nz>
2015-07-18 15:31:04 +1200

    separated prepared statement logic (but at what cost? :P ) from insert method, started update method

--- database.php
+++ database.php
@@ -65,39 +65,72 @@ class DatabaseHelper {
     *
     */
   public function insert($table, $formData){
-    $mysqli = $this->mysqli;
-    if(!$this->array_isAssoc($formData))
-        throw new DatabaseException("Form data needs to be an associative array! \n current:". print_r($formData, true));
-    foreach( $formData as $key => $value){
-      $values[] = $value;
-      $slots[] = "?";
-      $keys[] = $key;
+
+
+    $sql = "INSERT INTO `$table`({{COLUMNS}}) VALUES ({{VALUES}})";
+
+    // get values for header message
+    $values = array_values($formData);
+
+    try{
+      $this->secureQuery($sql,$formData);
+
+      $message = "message=Successfully added (".implode(',',$values).")";
     }
+    catch(Exception $error) {
+      $message = "Error=Failed to add (".implode(',',$values).")";
+      //$logger->log($error);
+    }
+    finally {
+      return $message;
+    }
+  }
+
+  /**
+    * prepares binds and executes a given mysql statement
+    * formatted:
+    * {{COLUMNS}} for columns to select
+    * {{values}} for values to add or update
+    * @param string $query query to execute
+    * @param associative array $data to insert into the sql statement
+    *  throws an error if not successful
+    */
+  private function secureQuery($sql,$data){
+    $mysqli = $this->mysqli;
+
+    if(!$this->array_isAssoc($data))
+        throw new DatabaseException("data needs to be an associative array! \n current:". print_r($formData, true));
+
+    $columns = array_keys($data);
+    $values = array_values($data);
+
+    // generate slots for each value
+    $slots = str_split( str_repeat("?", count($data)) );
 
 
-    $sql = "INSERT INTO `$table`(". implode(',', $keys) .") VALUES (". implode(',', $slots) .")";
-    if(!$statement = $mysqli->prepare($sql)){
+    // generate a value types string for the prepared statement
+    $types = implode('',$this->createTypesArray($values) );
+
+    // replace {{COLUMNS}}, {{VALUES}} and {{UPDATES}} with their actual values
+    $valuesToInsert = array( implode(',', $columns), implode(',', $slots), implode('=? ',$values)."=?" );
+    $stringsToReplace = array( "{{COLUMNS}}", "{{VALUES}}","{{UPDATES}}" );
+
+    $finalSql = str_replace($stringsToReplace, $valuesToInsert, $sql);
+
+    if(!$statement = $mysqli->prepare($finalSql)){
       throw new DatabaseException("Error prepearing statement:".$mysqli->error);
     }
-
-    // since all inserted values are going to be strings @TODO in future should make this take more data types
-    $types = implode('',$this->createTypesArray($values) );
 
     // function to insert the contents of an array as arguments to the bind_param function
     $bind_param_args = array_merge(array($types), $values);
 
     $query = call_user_func_array(array($statement, "bind_param"), $this->array_refValues($bind_param_args));
+    if(!$query) //throw a mysql error
+      throw new DatabaseException( "Error in mysql: ".$mysqli->error($query));
+
     $statement->execute();
 
-    // generate header message
-    if(!$query){ //throw a mysql error
-      throw new DatabaseException( "Error in mysql: ".$mysqli->error($query));
-      $message = "Error=Failed to add (".implode(',',$values).")";
-    }
-    else {
-      $message = "message=Successfully added (".implode(',',$values).")";
-    }
-    return $message;
+
   }
 
   /**
@@ -150,17 +183,72 @@ class DatabaseHelper {
 
   }
 
+
+
   /**
-  * retrieves contents of given columns from a given table
-  * @param array $table table in which to get rows from
-  * @param strings $columns... rows to get data from
-  * @return multi-dimensional array $rowData
-  */
+     * Enables updates to specific data in the database for a given table
+     *
+     * @param string $table Name of table to update in
+     * @param int $id Id of form row to update data within specified table
+     * @param associative array $formData data to insert into row of specified ID
+     *        with the format 'column => value'
+     *
+     * @TODO write update method
+     */
+  public function updateInTable($table,  $id, $formData){
+    $sql = "UPDATE `$table` SET {{UPDATES}} WHERE ID=?";
+
+    // get values for header message
+    $values = array_values($formData);
+    $formDataAndID = array_merge($formData,array($id));
+    try{
+      $this->secureQuery($sql,$formData);
+
+      $message = "message=Successfully updated (".implode(',',$values).")";
+    }
+    catch(Exception $error) {
+      $message = "Error=Failed to update (".implode(',',$values).")";
+      //$logger->log($error);
+    }
+    finally {
+      return $message;
+    }
+  }
+
+   /**
+   * removes a row of a given table based on given parameters
+   *
+   * @param string $table Name of table to remove row from
+   * @param string $column column to search for value
+   * @param string $value value in column of row or rows to remove
+   *
+   * @TODO write remove function
+   */
+  public function removeFromTable($table, $column, $value){
+    $sqlQuery = "DELETE FROM `$table` WHERE $column=$value";
+    try{
+      $result = $this->mysqli->query($sqlQuery);
+      $message = "$result row".(($result > 1)? "s" : "")." deleted \n";
+      return $message;
+    }
+    catch (Exception $error){
+      throw new DatabaseException($error);
+    }
+  }
+
+
+
+    /**
+    * retrieves contents of given columns from a given table
+    * @param array $table table in which to get rows from
+    * @param strings $columns... rows to get data from
+    * @return multi-dimensional array $rowData
+    */
   public function getRowsFromTable($table){
     if (func_num_args() > 1){
 
       $columnNames = array_slice(func_get_args(), 1);
-      $sqlQuery = "SELECT ".implode(", ", $columnNames)." FROM `$table`";
+      $sqlQuery = "SELECT ".implode(",", $columnNames)." FROM `$table`";
 
     }
     else {
@@ -168,11 +256,13 @@ class DatabaseHelper {
     }
 
     $result = $this->queryRows($sqlQuery);
+    // close $this->query
+    $this->query->close();
     return $result;
   }
 
   /**
-   * fetches an associative array of a single row from a table in the given sql.
+   * executes a given sql query on a row and returns an appropriate value.
    *
    * @param string $sql SQL to run against connected database
    * @return associative array $row Row matched by the given SQL statement
@@ -180,11 +270,15 @@ class DatabaseHelper {
   private function queryRow($sql = null){
     $mysqli = $this->mysqli;
     $this->query = $this->query ? : $mysqli->query($sql);
-    if(!$this->query)
+    if($mysqli->error)
           throw new DatabaseException( "Error in mysql: ".$mysqli->error);
 
+
+    // we have selected something
     $result = $this->query->fetch_assoc();
     return $result;
+
+
   }
   /**
     * calls the function queryRow for each row in a table and appends each
@@ -197,9 +291,9 @@ class DatabaseHelper {
     while($row = $this->queryRow($sql) ){
       $result[] = $row;
     }
+
     return $result;
   }
-
-
 }
+
 ?>
--- databaseTester.php
+++ databaseTester.php
@@ -6,6 +6,8 @@ class DatabaseTest extends PHPUnit_Framework_Testcase {
   public $username = "root";
   public $password = "Ch4ng3m3#";
   public $database = "website";
+
+  private $rows;
   /**
     * setup mysqli to test with
     */
@@ -22,8 +24,8 @@ class DatabaseTest extends PHPUnit_Framework_Testcase {
   public function testCanInsertIntoDatabase(){
     $databaseHelper = new DatabaseHelper();
 
-    $databaseHelper->insert("pages", array("title" => "contact", "body" => "parking can be found around the back.", "contacts_id" => 3, "edited_by" => 1));
-
+    $message = $databaseHelper->insert("pages", array("title" => "contact", "body" => "parking can be found around the back.", "contacts_id" => 3, "edited_by" => 1));
+    print_r("\n$message\n");
     $result = $this->mysqli->query("SELECT title, body, contacts_id, edited_by FROM `pages` WHERE `title` = 'contact'");
     $contact_page = $result->fetch_assoc();
     $this->assertEquals($contact_page['title'] , "contact");
@@ -35,16 +37,35 @@ class DatabaseTest extends PHPUnit_Framework_Testcase {
   public function testCanGetRowsFromTable(){
     $databaseHelper = new DatabaseHelper();
 
-    $rows = $databaseHelper->getRowsFromTable('pages', 'ID', 'title' ,'body' ,'contacts_id','edited_by');
-    foreach ($rows as $index => $row) {
+    $this->rows = $databaseHelper->getRowsFromTable('pages', 'ID', 'title' ,'body' ,'contacts_id','edited_by');
+
+    $this->assertGreaterThan(0,count($this->rows), "getRowsFromTable returned an empty array! :" . print_r($this->rows, TRUE));
+    foreach ($this->rows as $index => $row) {
       $this->assertArrayhasKey('ID' ,$row, "Could not find 'ID' in row.$index: ". print_r($row, TRUE));
       $this->assertArrayhasKey('title' ,$row, "Could not find 'title' in row.$index: ". print_r($row, TRUE));
       $this->assertArrayhasKey('body' ,$row, "Could not find 'body' in row.$index: ". print_r($row, TRUE));
       $this->assertArrayhasKey('contacts_id' ,$row, "Could not find 'contacts_id' in row.$index: ". print_r($row, TRUE));
       $this->assertArrayhasKey('edited_by' ,$row, "Could not find 'edited_by' in row.$index: ". print_r($row, TRUE));
     }
+  }
+
+  public function testCanRemoveFromTable(){
+
+    $this->mysqli->query("INSERT INTO `pages`(title, body, contacts_id, edited_by) VALUES ('about', 'At website we like to test to make sure our database is working.', 5, 12)");
+
+    $databaseHelper = new DatabaseHelper();
+    $this->rows = $databaseHelper->getRowsFromTable('pages');
+    foreach ($this->rows as $index => $row) {
+      $rowId = $row['ID'];
+      $message = $databaseHelper->removeFromTable('pages', 'ID' ,$rowId );
+      //print_r("\n$message\n");
+      $result = $this->mysqli->query("SELECT ID FROM `pages` WHERE ID = $rowId");
+      $contact_page = $result->fetch_assoc();
+      $this->assertEquals($contact_page, NULL, "Contains: ". print_r($contact_page, TRUE));
+    }
 
   }
+
 
 }
 ?>
