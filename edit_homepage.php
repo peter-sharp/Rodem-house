394874c9d2322d60de91caf92d9f670d24f53941
Peter Sharp <peter@petersharp.co.nz>
2015-07-21 20:58:01 +1200

    Tidied up form. moved breadcrumb to banner

--- edit_homepage.php
+++ edit_homepage.php
@@ -10,16 +10,26 @@ if($_POST['login'])   $database->updateInTable(`pages`, $meetingspage[0]['ID'], 
 <main class="editor">
   <div class="banner">
     <h1 class="col-md-4 center-block" >edit<br><small>home page</small></h1>
+    <p class="breadcrumb">website editor > home page</p>
   </div>
-  <article>
+  <article >
     <div class="container">
-      <p class="breadcrumb">website editor > home page</p>
-        <p class="user-type pull-right">logged in as <?= $_SESSION['usertype']?></p>
-        <label for="change[intro]">introduction text</label>
-        <textarea  id="name" name="change[intro]" ><?= $homepage[0]['body']?></textarea>
 
-        <a class="btn btn-back" name="change">back</a>
-        <button class="btn btn-CTA" type="submit" name="change">change</button>
+      <p class="user-type pull-right">logged in as <?= $_SESSION['usertype']?></p>
+      <div class="col-md-4 center-block">
+
+
+
+          <div class="form-group ">
+            <label for="">introduction text</label>
+            <textarea  id="name" name="change[intro]" class="form-control"><?= $homepage[0]['body']?></textarea>
+
+
+            <a class="btn btn-back" name="change">back</a>
+            <button class="btn btn-CTA pull-right" type="submit" name="change">change</button>
+
+          </div>
+      </div>
     </div>
   </article>
 </main>
