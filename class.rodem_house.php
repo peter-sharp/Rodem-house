0f327eb075d98b8ef5af3784a3c1732de711d338
Peter Sharp <Peter@petersharp.co.nz>
2015-07-23 11:25:11 +1200

    added method to get featured events fdorm database as well as property to store sql and columns

--- class.rodem_house.php
+++ class.rodem_house.php
@@ -8,8 +8,21 @@ class RodemHouse {
     public $currentPage; #@TODO needed for router-like functionality of index page
     public $pageNames = array('home','about us','meetings','contact', 'english lessons');
 
+    private $eventsSqlParams;
+
     public function RodemHouse(){
       $this->database = new DatabaseHelper();
+
+      $this->eventsSqlParams = array(
+        'columns' =>  array('ID','event_title','page_title','category_title','event_description',
+        'datetime','address_title','address','coordinates','image_title','image_description',
+        'image_location','featured'),
+
+        'joins' => "INNER JOIN `pages` ON `pages`.ID = `events`.page_id
+        INNER JOIN `categories` ON `categories`.ID = `events`.category_id
+        INNER JOIN `addresses` ON `addresses`.ID = `events`.address_id
+        INNER JOIN `images` ON `images`.ID = `events`.image_id"
+      );
     }
 
     /**
@@ -24,6 +37,52 @@ class RodemHouse {
       }
       else {
         throw new RodemHouseException("Error: $pageName is not a known page!");
+      }
+    }
+
+  /**
+ 	 * returns the current featured events of a given category or all featured events
+   * if no category is given
+ 	 *
+ 	 * @param string $category the category
+   * @return multidimensional array of events
+   *
+   */
+    public function getFeaturedEvents($category = null){
+      $columns = $this->eventsSqlParams['columns'];
+
+      $sql = $this->eventsSqlParams['joins']." WHERE `events`.featured = 1";
+
+      if (isset($category)){
+        $events = $database->getRowsFromTable('events',
+          $columns,
+          $sql." AND WHERE `categories`.category_title = $category");
+      }
+      else {
+        $events = $database->getRowsFromTable('events', $columns, $sql);
+      }
+    }
+
+    /**
+ 	 * return a category of a given title or all categories if none supplied
+ 	 *
+ 	 * @param string $title the title of the category name to return
+ 	 * @return mixed associative array if given a title or a multi dimensional array if not
+	 */
+
+    public function getCategories($title = null){
+      $columns = array('ID','category_title','category_description',
+      'address_title','address','coordinates');
+
+      $sql = " INNER JOIN `addresses` ON `addresses`.ID = `categories`.address_id";
+
+      if (isset($title)){
+        $events = $database->getRowsFromTable('categories',
+          $columns,
+          $sql." WHERE `categories`.category_title = $title");
+      }
+      else{
+        $events = $database->getRowsFromTable('events', $columns, $sql);
       }
     }
 
