e40ca9a6fba7de5a0f76c2cb55f55cbf4093d61d
Peter Sharp <Peter@petersharp.co.nz>
2015-07-24 09:41:29 +1200

    added icons to edit footer and $action check

--- templates/footer.php
+++ templates/footer.php
@@ -1,13 +1,13 @@
-<?php $edit_events = ($currentPage === 'edit_events');
+<?php $edit_events = ($currentPage === 'edit_events' && !isset($action));
 if ($edit_events && $authenticator->isAuthenticated()):?>
   <footer>
     <div class="container">
-      <p class="col-md-5 center-block">What would you like to do with the selected activities?</p>
-      <input type="submit" value="delete" >
+      <p class="col-md-6 center-block">What would you like to do with the selected activities?</p>
+      <img class="icon" src="./images/icons/delete.svg" alt="trash-can icon" /><input type="submit" name="action[delete]" value="delete" >
 
       <div class="pull-right">
-        <input type="submit" value="view" >
-        <input type="submit" value="edit" >
+        <img class="icon" src="./images/icons/view.svg" alt="binoculars icon" /><input type="submit" name="action[view]" value="view" >
+        <img class="icon" src="./images/icons/edit.svg" alt="pencil icon" /><input type="submit" name="action[edit]" value="edit" >
       </div>
     </div>
   </footer>
