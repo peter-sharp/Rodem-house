b2d63f76e168f609fcabeec33f566f4f40069b5e
Peter Sharp <Peter@petersharp.co.nz>
2015-07-24 09:49:47 +1200

    added accordion related classes and script-tag

--- meetings.php
+++ meetings.php
@@ -4,11 +4,15 @@ include('./templates/header.php');
 <main class="meetings">
   <div class="banner">
     <h1 class="col-md-4 center-block" >meetings</h1>
+    <noscript> Please enable javascript for full site functionality.</noscript>
   </div>
   <article class="meetings">
     <div class="container">
-      <h2>international fellowship</h2>
-      <section class="fellowship accordion">
+      <div class="accordion_tab fellowship">
+        <div class="arrow_right"></div>
+        <h2 class="">international fellowship</h2>
+      </div>
+      <section class="fellowship">
         <p>Enjoy a meal, friendship, and learn more about
           Christianity every Tuesday at 6pm.</p>
         <div class="event">
@@ -16,12 +20,16 @@ include('./templates/header.php');
           <P>This is the description for what will happen next Rodem fellowship.
           This could be a game night, guest speaker or any other type of event. It should
           be a reasonable description but not too long or else people might loose interest.</P>
-          <p class="map_caption">lessons are at 12 Some St</p>
+          <p class="map_caption">Find us at 12 Some St</p>
           <div class="map"></div>
         </div>
       </section>
-      <h2>social event</h2>
-      <section class="social accordion">
+
+      <div class="accordion_tab social">
+        <div class="arrow_right"></div>
+        <h2 class="">social event</h2>
+      </div>
+      <section class="social">
         <div class="event">
           <h3>Come ice-skating with us</h3>
           <P>This is the description for the up-comming social event.
@@ -35,6 +43,7 @@ include('./templates/header.php');
     </div>
   </article>
 </main>
+<script src="./scripts/accordion.js" type="text/javascript"></script>
 <?php
 include('./templates/footer.php');
 ?>
