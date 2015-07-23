d76076ab3e4c2fcee666e6102b82674e44f67be7
Peter Sharp <peter@petersharp.co.nz>
2015-07-23 15:39:33 +1200

    updated functions to be compatible with database class methods

--- class.rodem_house.administrator.php
+++ class.rodem_house.administrator.php
@@ -1,8 +1,9 @@
 <?php require_once("class.rodem_house.php");
 
 class RodemHouseAdmin extends RodemHouse {
-  public function RodemHouseAdmin(){
 
+  public function RodemHouseAdmin(){
+    parent::RodemHouse();
   }
 
   /**
@@ -12,20 +13,38 @@ class RodemHouseAdmin extends RodemHouse {
 	 */
 
   public function getEventList(){
-    $events = $this->database->getRowsFromTable('events', array('event_title','datetime','category_title','featured'), "INNER JOIN `categories` ON `categories`.ID = `events`. category_id");
+    $events = $this->database->getRowsFromTable('events', array('event_title','datetime','category_title','featured'));
     return $events;
   }
 
   /**
+ 	 * gets the values of given columns of the first event that matches the given conditions
  	 *
- 	 *
- 	 * @param type
- 	 * @return void
+ 	 * @param array $columns the columns to get values from
+   * @param associative array $conditions the conditions check rows against
+ 	 * @return associative array of columns and their values.
 	 */
+  public function getEvent( array $columns, array $conditions){
+    $event = $this->database->queryRow($database->buildSelectQuery('events', $columns, $conditions));
+    return $event;
+  }
 
-  public function getEvent( $column, $searchPropery){
-    if(isset($column) && isset($searchProperty))
-      $event = $this->database->queryRow("SELECT ");
+  /**
+ 	 * gets the values of given columns of the fisrt event that matches the given conditions
+ 	 *
+ 	 * @param string $pageName of page to get id from
+ 	 * @return string ID of page
+	 */
+  public function getPageID($pageName){
+    if (in_array($pageName, $this->pageNames)){
+      $sql = $this->database->buildSelectQuery("pages", array("`pages`.ID","page_title"), array( 'page_title' => $pageName));
+      $id = $this->database->queryRow($sql)['ID'];
+      //die(var_dump($id));
+      return $id;
+    }
+    else {
+      throw new RodemHouseException("Error: $pageName is not a known page!");
+    }
   }
 
 }
