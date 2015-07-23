1fa3153eba953fb6ebb8e98d1e67a305866484a3
Peter Sharp <Peter@petersharp.co.nz>
2015-07-24 09:48:50 +1200

    tidied up with bootstrap. fixed some html tag typos fixed broken php added php to change action button label and class

--- _event_form.php
+++ _event_form.php
@@ -1,51 +1,53 @@
-<div class="form-group">
-  <label for="category">Category</label>
-  <?php if(!$readonly): ?>
-    <select  name="category" >
-      <option value="fellowship">Rodem Fellowship</option>
-      <option value="social">Social event</option>
-    </select>
-  <?php else: ?>
-    <input  type="text" name="category" READONLY value="<?= $edit_event['category']?>">
-  <?php endif ?>
-</div>
-
-<div class="form-group">
-  <label for="title">Title</label>
-  <input  type="text" name="title" <? if($readonly) echo 'READONLY' ?>value="<?= $edit_event['title']?>">
-</div>
-
-<div class="form-group">
-  <label for="image">Image to display</label>
-  <?php if(!$readonly): ?>
-    <select  name="image" >
-      <option value="speaker">Guest speaker</option>
-      <option value="coffee">Coffee and talk</option>
-    </select>
-  <?php else: ?>
-    <img  name="image"  src="<?= $edit_event['image']['location']?>" alt="<?= $edit_event['image']['description']?>">
-  <?php endif ?>
-</div>
-
-<div class="form-group">
-  <label for="description">Description</label>
-  <textarea name="description" <? if($readonly) echo 'READONLY' ?>><?= $edit_event['description']?></textarea>
+<div class="col-md-5 center-block">
+  <div class="form-group">
+    <label for="category">Category</label>
+    <?php if(!$readonly): ?>
+      <select  name="category"  class="form-control">
+        <option value="fellowship">Rodem Fellowship</option>
+        <option value="social">Social event</option>
+      </select>
+    <?php else: ?>
+      <input  type="text" name="category" READONLY value="<?= $edit_event['category']?>" class="form-control">
+    <?php endif ?>
   </div>
 
-<div class="form-group">
-  <label for="address">Address</label>
-  <input  type="text" name="address" <? if($readonly) echo 'READONLY' ?>value="<?= $edit_event['address']?>">
-</div>
+  <div class="form-group">
+    <label >Title</label>
+    <input  type="text" name="title" <?= ($readonly)? 'READONLY' : '' ?>value="<?= $edit_event['title']?>" class="form-control">
+  </div>
 
-<div class="form-group">
-  <label for="datetime">Time</label>
-  <input  type="datetime-local" name="datetime" <? if($readonly) echo 'READONLY' ?>value="<?= $edit_event['datetime']?>">
-</div>
+  <div class="form-group">
+    <label >Image to display</label>
+    <?php if(!$readonly): ?>
+      <select  name="image"  class="form-control">
+        <option value="speaker">Guest speaker</option>
+        <option value="coffee">Coffee and talk</option>
+      </select>
+    <?php else: ?>
+      <img  name="image"  src="<?= $edit_event['image']['location']?>" alt="<?= $edit_event['image']['description']?>">
+    <?php endif ?>
+  </div>
 
-<div class="form-group">
-  <label for="featured">Featured?</label>
-  <input  type="checkbox" name="featured" <? if($readonly) echo 'READONLY' ?>value="<?= $edit_event['featured']?>">
-</div>
+  <div class="form-group">
+    <label >Description</label>
+    <textarea name="description" <?= ($readonly)? 'READONLY' : '' ?> class="form-control"><?= $edit_event['description']?></textarea>
+    </div>
 
-<a class="btn btn-back" name="change">back</button>
-<button class="btn btn-CTA" type="submit" name="change"><?= $actionType?></button>
+  <div class="form-group">
+    <label >Address</label>
+    <input  type="text" name="address" <?= ($readonly)? 'READONLY' : '' ?>value="<?= $edit_event['address']?>" class="form-control">
+  </div>
+
+  <div class="form-group">
+    <label >Time</label>
+    <input  type="datetime-local" name="datetime" <?= ($readonly)? 'READONLY' : '' ?>value="<?= $edit_event['datetime']?>" class="form-control">
+  </div>
+
+  <div class="form-group">
+    <label for="featured">Featured?</label>
+    <input  type="checkbox" name="featured" <?= ($readonly)? 'READONLY' : '' ?>value="<?= $edit_event['featured']?>" >
+  </div>
+
+  <a class="btn btn-back" name="change">back</a>
+  <input class="btn <?= ($action['type'] === 'add')? 'btn-ADD' : 'btn-CTA'?> pull-right" type="submit" name="change" value="<?= $action['type']?>">
+</div>
