065aefac9f299834f389a5b09c9201c3b384d920
Peter Sharp <Peter@petersharp.co.nz>
2015-07-24 09:41:45 +1200

    added icons

--- contact.php
+++ contact.php
@@ -9,16 +9,16 @@ include('./templates/header.php');
   <article itemscope itemtype="http://schema.org/Person">
     <div class="container">
       <p>
-        <img src="" alt="" /> 022 333 4444
+        <img src="./images/icons/mobile.svg" alt="mobile phone icon" /> 022 333 4444
       </p>
       <p>
-        <img src="" alt="" /> 03 444 5556
+        <img src="./images/icons/phone.svg" alt="phone handset icon" /> 03 444 5556
       </p>
       <p>
-        <img src="" alt="" /> person@rodemhouse.org
+        <img src="./images/icons/letter.svg" alt="letter icon" /> person@rodemhouse.org
       </p>
       <p>
-        <img src="" alt="" /> Some St, Suburb, Christchurch
+        <img src="./images/icons/map_pin.svg" alt="map pin icon" /> Some St, Suburb, Christchurch
       </p>
       <div class="map"></div>
     </div>
