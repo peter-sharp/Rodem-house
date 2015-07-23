3b93ba1124caef9c0a8df9ad3df06fd61b5ee427
Peter Sharp <Peter@petersharp.co.nz>
2015-07-23 19:04:13 +1200

    Added database content loading

--- about.php
+++ about.php
@@ -1,5 +1,7 @@
 <?php
 include('./templates/header.php');
+$aboutBody = $rodemHouse->getPageContent('about us')['body'];
+
 ?>
 <main class="about">
   <div class="banner">
@@ -8,10 +10,14 @@ include('./templates/header.php');
   <article>
     <div class="container">
       <div class="site wrapper">
+        <?php if($aboutBody):
+                echo $aboutBody;
+        else: ?>
         <h2>who we are</h2>
         <p>Rodem House is a charitable trust organization <small>(Registration No: CC10913)</small> to support missionaries
           and pastors who need rest and refreshment. Also we support international people who come to
           Christchurch. Rodem house is a faith mission operated by prayer and the support of Christians.</p>
+        <?php endif?>
       </div>
     </div>
   </article>
