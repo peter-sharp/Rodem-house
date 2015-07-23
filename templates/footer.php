3707e6ae4f59b3ba89c7fa1e5c0545c0278bf2ab
Peter Sharp <Peter@petersharp.co.nz>
2015-07-23 19:13:00 +1200

    reorganised edit footer added bootstrap classes

--- templates/footer.php
+++ templates/footer.php
@@ -2,10 +2,13 @@
 if ($edit_events && $authenticator->isAuthenticated()):?>
   <footer>
     <div class="container">
-      <p>What would you like to do with the selected activities?</p>
-      <button type="button">delete</button>
-      <button type="button">view</button>
-      <button type="button">edit</button>
+      <p class="col-md-5 center-block">What would you like to do with the selected activities?</p>
+      <input type="submit" value="delete" >
+
+      <div class="pull-right">
+        <input type="submit" value="view" >
+        <input type="submit" value="edit" >
+      </div>
     </div>
   </footer>
 </form>
