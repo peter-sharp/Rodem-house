0ca2e18ee5a1bc50dad255da906b68ebf06cc487
peter-sharp <Peter@petersharp.co.nz>
2015-07-28 11:18:30 +1200

    added maps

--- meetings.php
+++ meetings.php
@@ -1,6 +1,8 @@
 <?php
 include('./templates/header.php');
+#TODO$categories = $rodemHouse->getCategoryInfo();
 ?>
+<script src="https://maps.googleapis.com/maps/api/js"></script>
 <main class="meetings">
   <div class="banner">
     <h1 class="col-md-4 center-block" >meetings</h1>
@@ -20,8 +22,11 @@ include('./templates/header.php');
           <P>This is the description for what will happen next Rodem fellowship.
           This could be a game night, guest speaker or any other type of event. It should
           be a reasonable description but not too long or else people might loose interest.</P>
-          <p class="map_caption">Find us at 12 Some St</p>
-          <div class="map"></div>
+          <p class="map_caption">Find us at 344 Manchester St, Christchurch Central, Richmond 8013</p>
+          <div class="map" id="Rodem Fellowship"
+          data-location-x="-43.518982"
+          data-location-y="172.640104"
+          data-zoom="17"></div>
         </div>
       </section>
 
@@ -36,12 +41,16 @@ include('./templates/header.php');
           Here you can put some text to interest people in coming an other details
           like what they need to bring, what will happen if itrains etc.</P>
           <p class="map_caption">Be there by 5:45pm at Some other St</p>
-          <div class="map"></div>
+          <div class="map" id="Alpine Ice Sports Centre"
+          data-location-x="-43.547628"
+          data-location-y="172.657576"
+          data-zoom="17"></div>
         </div>
 
       </section>
     </div>
   </article>
+  <script src="./scripts/maps.js"></script>
 </main>
 <script src="./scripts/accordion.js" type="text/javascript"></script>
 <?php
