f38f5576d18dcc57209319e43fd7522a3e7dbd37
peter-sharp <Peter@petersharp.co.nz>
2015-07-28 11:10:39 +1200

    changed structue of post array

--- _event_form.php
+++ _event_form.php
@@ -2,9 +2,9 @@
   <div class="form-group">
     <label for="category">Category</label>
     <?php if(!$readonly): ?>
-      <select  name="category"  class="form-control">
-        <option value="fellowship">Rodem Fellowship</option>
-        <option value="social">Social event</option>
+      <select  name="<?= $action['type']?>[category]"  class="form-control">
+        <option value="fellowship">rodem fellowship</option>
+        <option value="social">social events</option>
       </select>
     <?php else: ?>
       <input  type="text" name="category" READONLY value="<?= $edit_event['category']?>" class="form-control">
@@ -13,13 +13,13 @@
 
   <div class="form-group">
     <label >Title</label>
-    <input  type="text" name="title" <?= ($readonly)? 'READONLY' : '' ?>value="<?= $edit_event['title']?>" class="form-control">
+    <input  type="text" name="<?= $action['type']?>[title]" <?= ($readonly)? 'READONLY' : '' ?>value="<?= $edit_event['title']?>" class="form-control">
   </div>
 
   <div class="form-group">
     <label >Image to display</label>
     <?php if(!$readonly): ?>
-      <select  name="image"  class="form-control">
+      <select  name="<?= $action['type']?>[image]"  class="form-control">
         <option value="speaker">Guest speaker</option>
         <option value="coffee">Coffee and talk</option>
       </select>
@@ -30,24 +30,24 @@
 
   <div class="form-group">
     <label >Description</label>
-    <textarea name="description" <?= ($readonly)? 'READONLY' : '' ?> class="form-control"><?= $edit_event['description']?></textarea>
+    <textarea name="<?= $action['type']?>[description]" <?= ($readonly)? 'READONLY' : '' ?> class="form-control"><?= $edit_event['description']?></textarea>
     </div>
 
   <div class="form-group">
     <label >Address</label>
-    <input  type="text" name="address" <?= ($readonly)? 'READONLY' : '' ?>value="<?= $edit_event['address']?>" class="form-control">
+    <input  type="text" name="<?= $action['type']?>[address]" <?= ($readonly)? 'READONLY' : '' ?>value="<?= $edit_event['address']?>" class="form-control">
   </div>
 
   <div class="form-group">
     <label >Time</label>
-    <input  type="datetime-local" name="datetime" <?= ($readonly)? 'READONLY' : '' ?>value="<?= $edit_event['datetime']?>" class="form-control">
+    <input  type="datetime-local" name="<?= $action['type']?>[datetime]" <?= ($readonly)? 'READONLY' : '' ?>value="<?= $edit_event['datetime']?>" class="form-control">
   </div>
 
   <div class="form-group">
     <label for="featured">Featured?</label>
-    <input  type="checkbox" name="featured" <?= ($readonly)? 'READONLY' : '' ?>value="<?= $edit_event['featured']?>" >
+    <input  type="checkbox" name="<?= $action['type']?>[featured]" <?= ($readonly)? 'READONLY' : '' ?>value="<?= $edit_event['featured']?>" >
   </div>
 
   <a class="btn btn-back" name="change">back</a>
-  <input class="btn <?= ($action['type'] === 'add')? 'btn-ADD' : 'btn-CTA'?> pull-right" type="submit" name="change" value="<?= $action['type']?>">
+  <input class="btn <?= ($action['type'] === 'add')? 'btn-ADD' : 'btn-CTA'?> pull-right" type="submit" name="submit" value="<?= $action['type']?>">
 </div>
