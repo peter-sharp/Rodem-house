7f5ccca2f9206aa96398bc02cd2439aa6aedd375
peter-sharp <Peter@petersharp.co.nz>
2015-07-28 11:17:32 +1200

    added map

--- english.php
+++ english.php
@@ -1,6 +1,7 @@
 <?php
 include('./templates/header.php');
 ?>
+<script src="https://maps.googleapis.com/maps/api/js"></script>
 <main class="english">
   <div class="banner">
     <h1 class="col-md-4 center-block" >english lessons</h1>
@@ -11,10 +12,15 @@ include('./templates/header.php');
       <p>Classes are relaxed and fun. Our tutor Nancy will help you practice and improve your <strong>speaking</strong>,
       <strong>vocabulary</strong> and <strong>grammar</strong> skills.</p>
       <p>English learners of all abilities are always welcome.</p>
-      <p class="map_caption">lessons are at 12 Some St</p>
-      <div class="map"></div>
+      <p class="map_caption">lessons are at 344 Manchester St, Christchurch Central, Richmond 8013</p>
+
+      <div class="map" id="Rodem Fellowship"
+      data-location-x="-43.518982"
+      data-location-y="172.640104"
+      data-zoom="17"></div>
     </div>
   </article>
+  <script src="./scripts/maps.js"></script>
 </main>
 <?php
 include('./templates/footer.php');
