a0cf3f7dd259737f3cb2af0668d27d7a7fcf78b6
peter-sharp <Peter@petersharp.co.nz>
2015-07-28 11:17:19 +1200

    moved breadcrumbs to the right place, pulled change button right

--- edit_meetingspage.php
+++ edit_meetingspage.php
@@ -9,17 +9,18 @@ if($_POST['login'])   $database->updateInTable(`pages`, $meetingspage[0]['ID'], 
 <main class="editor">
   <div class="banner">
     <h1 class="col-md-4 center-block" >edit<br><small>meetings page</small></h1>
+    <p class="breadcrumb">website editor > meetings page</p>
   </div>
   <article>
     <div class="container">
-      <p class="breadcrumb">website editor > meetings page</p>
+
         <p class="user-type pull-right">logged in as <?= $_SESSION['usertype']?></p>
         <h2>rodem fellowship meeting</h2>
         <label for="change[intro]">Rodem fellowship introduction text</label>
         <textarea  id="name" name="change[intro]" ><?= $meeingspage[0]['body']?></textarea>
 
         <a class="btn btn-back" name="change">back</a>
-        <button class="btn btn-CTA" type="submit" name="change">change</button>
+        <button class="btn btn-CTA pull-right" type="submit" name="change">change</button>
     </div>
   </article>
 </main>
